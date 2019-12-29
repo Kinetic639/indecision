3a8483e8b57df5f5a7318be07bbf1664cb05f76a
Michal Stepien <michal.stepien36@gmail.com>
2019-12-29 09:17:52 +0100

    mediaQueries, favicon

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Modal from "react-modal";
+import ReactModal from "react-modal";
+ReactModal.setAppElement("#root");
 
 export default function OptionModal(props) {
   return (
@@ -8,11 +10,15 @@ export default function OptionModal(props) {
       contentLabel="Selected Option"
       onRequestClose={props.okClick}
       closeTimeoutMS={200}
-      className='modal'
+      className="modal"
     >
-      <h3 className='modal__title'>Selected option</h3>
-      {props.selectedOption && <p className='modal__body'>{props.selectedOption}</p>}
-      <button className='button' onClick={props.okClick}>OK</button>
+      <h3 className="modal__title">Selected option</h3>
+      {props.selectedOption && (
+        <p className="modal__body">{props.selectedOption}</p>
+      )}
+      <button className="button" onClick={props.okClick}>
+        OK
+      </button>
     </Modal>
   );
 }
