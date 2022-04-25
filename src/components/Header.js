8df74a972200af9a830e83d38084b7430f7a230a
Kinetic639 <michal.stepien36@gmail.com>
2022-04-25 13:08:14 +0200

    make it better

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,16 @@
-import React from "react";
+import React from 'react';
 
-const Header = props => (
+const Header = (props) => (
   <div className="header">
-   <div className='container'>
+    <div className="container">
       <h1 className="header__title">{props.title}</h1>
       {props.subtitle && <h2 className="header__subtitle">{props.subtitle}</h2>}
-   </div>
+    </div>
   </div>
 );
 
 Header.defaultProps = {
-  title: "Indecision"
+  title: 'Random value Picker',
 };
 
 export default Header;
